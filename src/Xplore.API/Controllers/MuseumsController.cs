fb7f92e52b5bb5ff4ab6a5af3899ae35f5a5cb33
FabboCassa <casarini.fabio@gmail.com>
2026-01-29 15:26:37 +0100

    Created first api

---
+++ src/Xplore.API/Controllers/MuseumsController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Xplore.Domain.Entities;
+using Xplore.Infrastructure.Persistence;
+
+namespace Xplore.API.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class MuseumsController : ControllerBase
+{
+    private readonly ApplicationDbContext _context;
+
+    // Iniettiamo il DbContext che abbiamo configurato prima
+    public MuseumsController(ApplicationDbContext context)
+    {
+        _context = context;
+    }
+
+    // GET: api/museums
+    [HttpGet]
+    public async Task<IActionResult> GetAll()
+    {
+        var museums = await _context.Museums.ToListAsync();
+        return Ok(museums);
+    }
+
+    // POST: api/museums
+    [HttpPost]
+    public async Task<IActionResult> Create(Museum museum)
+    {
+        // 1. Aggiungi al contesto
+        _context.Museums.Add(museum);
+
+        // 2. Salva nel DB vero (Commit)
+        await _context.SaveChangesAsync();
+
+        return CreatedAtAction(nameof(GetAll), new { id = museum.Id }, museum);
+    }
+}
