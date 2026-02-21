bdc3a15977047acf75096f6ded4c8ab49f68b7d0
FabboCassa <casarini.fabio@gmail.com>
2026-02-21 23:21:42 +0100

    Ficed login, needs reset security parameters

--- src/Xplore.API/Program.cs
+++ src/Xplore.API/Program.cs
@@ -21,11 +21,11 @@ builder.AddNpgsqlDbContext<ApplicationDbContext>("xploredb");
 builder.Services.AddIdentity<ApplicationUser, IdentityRole>(options =>
 {
     // Password settings
-    options.Password.RequireDigit = true;
-    options.Password.RequiredLength = 8;
-    options.Password.RequireNonAlphanumeric = true;
-    options.Password.RequireUppercase = true;
-    options.Password.RequireLowercase = true;
+    options.Password.RequireDigit = false;
+    options.Password.RequiredLength = 4;
+    options.Password.RequireNonAlphanumeric = false;
+    options.Password.RequireUppercase = false;
+    options.Password.RequireLowercase = false;
 
     // User settings
     options.User.RequireUniqueEmail = true;
